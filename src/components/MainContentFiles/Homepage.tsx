d4ee99c64bc2d467ca12a2ccac0d17b748b76a9e
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-01-08 23:19:42 +0000

    add styling to home page

--- src/components/MainContentFiles/Homepage.tsx
+++ src/components/MainContentFiles/Homepage.tsx
@@ -7,29 +7,29 @@ export default function Homepage({
   handleSubmitPaste,
 }: IHomepage): JSX.Element {
   return (
-    <>
-      <div className="pasteContainer">
-        <form
-          onSubmit={(e) => {
-            handleSubmitPaste();
-            e.preventDefault();
-          }}
-        >
-          <input
-            type="text"
-            placeholder="Title (optional)"
-            value={pasteInput.title}
-            name="title"
-            onChange={(e) => setPasteInput(e)}
-          />
-          <textarea
-            name="pasteSummary"
-            value={pasteInput.body}
-            onChange={(e) => setPasteInput(e)}
-          ></textarea>
-          <input type="submit" />
-        </form>
-      </div>
-    </>
+    <div className="pasteContainer">
+      <form
+        onSubmit={(e) => {
+          handleSubmitPaste();
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Title (optional)"
+          value={pasteInput.title}
+          name="title"
+          onChange={(e) => setPasteInput(e)}
+          className="titleInput"
+        />
+        <textarea
+          name="pasteSummary"
+          value={pasteInput.body}
+          onChange={(e) => setPasteInput(e)}
+          placeholder="Enter Paste"
+        ></textarea>
+        <input type="submit" className="submitPaste" />
+      </form>
+    </div>
   );
 }
