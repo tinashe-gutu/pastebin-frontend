659a75fa458905705f789097d857f0f20e8ce75a
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-01-08 22:27:30 +0000

    change backend server URL

--- src/utils/envBaseURL.ts
+++ src/utils/envBaseURL.ts
@@ -1,4 +1,4 @@
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://zacgladman-zac-tinashe-pastebin-backend.onrender.com"
+    ? "https://pastebin-backend.onrender.com"
     : "http://localhost:4000";
